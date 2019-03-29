3d27c91251dca30540d6413ab2d27d86194698e0
SERVERS\paw.byrialsen <paw.byrialsen@stevanatogroup.com>
2019-03-29 10:39:21 +0100

    Created project

---
+++ ModuleInitializer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatelLobDemo
+{
+    /// <summary>
+    /// Used by the ModuleInit. All code inside the Initialize method is ran as soon as the assembly is loaded.
+    /// </summary>
+    public static class ModuleInitializer
+    {
+        /// <summary>
+        /// Initializes the module.
+        /// </summary>
+        public static void Initialize()
+        {
+            // register services. Important to register navgiation related services first
+            RegisterNavigation();
+            RegisterServices();
+        }
+
+        /// <summary>
+        /// Register services
+        /// </summary>
+        private static void RegisterServices()
+        {
+        }
+
+        /// <summary>
+        /// Register navigation
+        /// </summary>
+        private static void RegisterNavigation()
+        {
+        }
+    }
+}
